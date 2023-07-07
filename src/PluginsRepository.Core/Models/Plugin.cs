92a709f83f91c372c9e4ca3246671984fdbaacd4
ArchLeaders <archparliament@gmail.com>
2023-07-07 13:34:17 -0700

    Create plugin model

---
+++ src/PluginsRepository.Core/Models/Plugin.cs
@@ -0,0 +1,31 @@
+﻿namespace PluginsRepository.Core.Models;
+
+public partial class Plugin : TableItem<Plugin>
+{
+    [ObservableProperty]
+    private string _name = string.Empty;
+
+    [ObservableProperty]
+    private string _description = string.Empty;
+
+    [ObservableProperty]
+    private string _assemblyName = string.Empty;
+
+    [ObservableProperty]
+    private string _organization = string.Empty;
+
+    [ObservableProperty]
+    private string _repository = string.Empty;
+
+    [ObservableProperty]
+    private List<Comment> _comments = new();
+
+    [ObservableProperty]
+    private DateTimeOffset _createdAt = DateTimeOffset.UtcNow;
+
+    [ObservableProperty]
+    private long _authorId;
+
+    [ObservableProperty]
+    private long _issueId;
+}
