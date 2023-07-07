f7105e5bf85027bc041b7ec4b4e3f63e12c7ee8e
ArchLeaders <archparliament@gmail.com>
2023-07-07 12:39:13 -0700

    Define IConnection interface - Impl IConnection in OctokitConnection

---
+++ PluginsLibrary.Core/Components/IConnection.cs
@@ -0,0 +1,6 @@
+﻿namespace PluginsRepository.Core.Components;
+
+public interface IConnection
+{
+    Dictionary<Guid, T> GetResource<T>() where T : TableItem<T>;
+}
---
+++ PluginsLibrary.Core/Components/OctokitConnection.cs
@@ -0,0 +1,23 @@
+﻿using Octokit;
+
+namespace PluginsRepository.Core.Components;
+
+public class OctokitConnection : IConnection
+{
+    private readonly HttpClient _client = new();
+    private readonly GitHubClient _githubClient = new(new ProductHeaderValue("public-plugins-repository"));
+
+    private const string Organization = "ArchLeaders";
+    private const string Repository = "PluginsRepository";
+
+    /// <summary>
+    /// Looks 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    /// <exception cref="NotImplementedException"></exception>
+    public Dictionary<Guid, T> GetResource<T>() where T : TableItem<T>
+    {
+        throw new NotImplementedException();
+    }
+}
