340e4244b2cde9ce246b2a952f0dccc8c91dba34
ArchLeaders <archparliament@gmail.com>
2023-07-07 13:34:12 -0700

    Create comment model

---
+++ src/PluginsRepository.Core/Models/Comment.cs
@@ -0,0 +1,13 @@
+﻿namespace PluginsRepository.Core.Models;
+
+public partial class Comment : ObservableObject
+{
+    [ObservableProperty]
+    private string _content = string.Empty;
+
+    [ObservableProperty]
+    private long _authorId;
+
+    [ObservableProperty]
+    private DateTimeOffset _createdAt = DateTimeOffset.UtcNow;
+}
